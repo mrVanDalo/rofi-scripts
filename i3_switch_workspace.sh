0ce19e284befc0e633305055a07cbe762e665d6f
Ingolf Wagner <contact@ingolf-wagner.de>
2022-10-13 11:36:57 +0200

    using jq instead of sed
    Thanks, for the input. I like to use jq instead.

--- i3_switch_workspace.sh
+++ i3_switch_workspace.sh
@@ -1,17 +1,10 @@
-#!/bin/bash
-
-function gen_workspaces()
-{
-    i3-msg -t get_workspaces | tr ',' '\n' | grep "name" | sed 's/"name":"\(.*\)"/\1/g' | sort -n
-}
-
-
-WORKSPACE=$( (echo empty; gen_workspaces)  | rofi -dmenu -p "Select workspace:")
-
-if [ x"empty" = x"${WORKSPACE}" ]
-then
-    i3_empty_workspace.sh
-elif [ -n "${WORKSPACE}" ]
-then
-    i3-msg workspace "${WORKSPACE}"
-fi
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+i3-msg -t get_workspaces | \
+jq --raw-output '.[] | .name' | \
+rofi -dmenu -p 'Select Workspace' | \
+while read line
+do
+  i3-msg workspace "$line"
+done
